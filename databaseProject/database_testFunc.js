4a099ea1896cd18d2d2c1678a679e4067d8f64bf
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-24 12:15:24 +0200

    update

--- databaseProject/database_testFunc.js
+++ databaseProject/database_testFunc.js
@@ -27,38 +27,59 @@ async function getAll() {
 async function run() {
   await getAll();
   await add({
-    id:200,
-    firstname:"Mike",
-    lastname:"jones",
-    department:"maints",
-    salary:4000
-  })
-  await getAll();
-  await remove(200)
+    id: 200,
+    firstname: "Mike",
+    lastname: "jones",
+    department: "maints",
+    salary: 4000,
+  });
+
+  await update({
+    id: 200,
+    firstname: "Mikex",
+    lastname: "jones",
+    department: "maintsx",
+    salary: 4000,
+  });
   await getAll();
 }
-async function remove(id){
- try {
-  const status = await db.doQuery("delete from employee where id=?", [id])
-  console.log(status)
- } catch (error) {
-  
- }
+async function remove(id) {
+  try {
+    const status = await db.doQuery("delete from employee where id=?", [id]);
+    console.log(status);
+  } catch (error) {}
 }
-async function add(employee){
-  try{
+async function add(employee) {
+  try {
     const parameters = [
       employee.id,
       employee.firstname,
       employee.lastname,
       employee.department,
-      employee.salary
-    ]
-    const sql = "insert into employee values(?,?,?,?,?)"
-    const status = await db.doQuery(sql, parameters)
-    console.log(status)
+      employee.salary,
+    ];
+    const sql = "insert into employee values(?,?,?,?,?)";
+    const status = await db.doQuery(sql, parameters);
+    console.log(status);
+  } catch (err) {
+    console.log(err);
   }
-  catch(err){
-    console.log(err)
+}
+
+async function update(modifiedEmployee) {
+  try {
+    const sql =
+      "update employee set firstname=?, lastname=?, department=?, salary=? where id=?";
+    const parameters = [
+      modifiedEmployee.firstname,
+      modifiedEmployee.lastname,
+      modifiedEmployee.department,
+      modifiedEmployee.salary,
+      modifiedEmployee.id,
+    ];
+    const status = await db.doQuery(sql, parameters);
+    console.log(status);
+  } catch (error) {
+    console.log(error);
   }
 }
