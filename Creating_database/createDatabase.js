439e3ca6200ff5da0ca8fcc2f3d90a38ef6abd90
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-31 10:56:01 +0200

    .

--- Creating_database/createDatabase.js
+++ Creating_database/createDatabase.js
@@ -1,12 +1,28 @@
-const Database = require("./database")
-let createStatementFile = "./createStatements.json"
-let adminPass ="";
-
-if (process.argv.length > 2){
-    adminPass=process.argv[2];
-    if(process.argv.length>3){
-        createStatementFile=`./${process.argv[3]}`;
-    }
+const Database = require("./database");
+let createStatementFile = "./createStatements.json";
+let adminPass = "";
+const printMessage = (message) => console.log(message);
+const printStatement = (statement) => printMessage(`${statement}`);
+const printError = (message) =>
+  printError(`${"#".repeat(20)} Error ${"#".repeat(20)}`);
+if (process.argv.length > 2) {
+  adminPass = process.argv[2];
+  if (process.argv.length > 3) {
+    createStatementFile = `./${process.argv[3]}`;
+  }
 }
 
-console.log(createStatementFile, adminPass)
+try {
+  console.log(require(createStatementFile), adminPass);
+} catch (error) {
+  console.log(error.message);
+}
+
+async function createdb(createStatements, adminPass) {
+  const options = {
+    host: createStatements.host,
+    port: createStatements.port,
+    user: createStatements.user,
+    password: createStatements.password,
+  };
+}
