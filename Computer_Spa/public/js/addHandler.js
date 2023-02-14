df2432e87dfce7d9b0532c051bd5f2978a4d43c4
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-14 13:15:08 +0200

    add handler

--- Computer_Spa/public/js/addHandler.js
+++ Computer_Spa/public/js/addHandler.js
@@ -1,10 +1,24 @@
 (function(){
+    let idField;
+    let nameField;
+    let typeField;
+    let processorFiled;
+    let amountField;
+    document.addEventListener("DOMContentLoaded", init)
+    function init(){
+        idField = document.getElementById("id")
+        nameField = document.getElementById("name")
+        typeField = document.getElementById("type")
+        processorFiled = document.getElementById("processor")
+        amountField = document.getElementById("amount")
+        document.getElementById("sumbit").addEventListener("click", send)
+    }
     async function send(){
         const computer={
             id: idField.value,
             name: nameField.value,
             type: typeField.value,
-            processor: processor.value,
+            processor: processorFiled.value,
             amount: +amountField.value
         }
         try {
@@ -16,6 +30,8 @@
                 },
                 mode:"cors"
             }
+            const data = await fetch("http://localhost:4000/api/computers", options)
+            const status = data.json();
             
         } catch (error) {
             
