03c0c851b6d0b3056d4b306a28dd2b5c1a21a3da
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-14 10:52:47 +0200

    add new

---
+++ Computer_Spa/public/js/addHandler.js
@@ -0,0 +1,11 @@
+(function(){
+    async function send(){
+        const computer={
+            id: idField.value,
+            name: nameField.value,
+            type: typeField.value,
+            processor: processor.value,
+            amount: +amountField.value
+        }
+    }
+})();
